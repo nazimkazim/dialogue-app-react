68fc2f0415d6937b7e6e07bef50593409a0b7801
Your Name <nazik_111@mail.ru>
2019-07-01 11:55:35 +0600

    add tooltip component

--- src/BubbleSpeechFrame.js
+++ src/BubbleSpeechFrame.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import Tooltip from './Tooltip';
 
 class BubbleSpeechFrame extends Component {
   constructor(props) {
     super(props);
     this.showText = this.showText.bind(this);
-    this.state = {
+    /* this.state = {
       show: false
-    };
+    }; */
   }
 
   showText(e) {
@@ -26,7 +27,12 @@ class BubbleSpeechFrame extends Component {
           {line.speaker === 1 ? (
             <div className="speaker-1" key={line.id}>
               <div className="sound-cont">
-                <ReactAudioPlayer src={line.audio} autoPlay controls />
+                <ReactAudioPlayer
+                  src={line.audio}
+                  autoPlay
+                  controls
+                  controlsList="nodownload"
+                />
               </div>
               <div className="text-cont">
                 {<p className="text">{line.text}</p>}
@@ -41,11 +47,17 @@ class BubbleSpeechFrame extends Component {
                   show text
                 </button>
               </div>
+              {line.helpers && <Tooltip tips={line.helpers} />}
             </div>
           ) : (
             <div className="speaker-2" key={line.id}>
               <div className="sound-cont">
-                <ReactAudioPlayer src={line.audio} autoPlay controls />
+                <ReactAudioPlayer
+                  src={line.audio}
+                  autoPlay
+                  controls
+                  controlsList="nodownload"
+                />
               </div>
               <div className="text-cont">
                 {<p className="text">{line.text}</p>}
@@ -60,6 +72,7 @@ class BubbleSpeechFrame extends Component {
                   show text
                 </button>
               </div>
+              {line.helpers && <Tooltip tips={line.helpers} />}
             </div>
           )}
         </React.Fragment>
---
+++ src/Tooltip.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import helpIcon from './help.svg';
+
+function Tooltip(props) {
+  console.log(props.tips);
+  return (
+    <div className="tooltip">
+      <div className="helpIcon">
+        <img src={helpIcon} alt="help icon" />
+      </div>
+      <div className="tip">
+        {props.tips &&
+          props.tips.map(tip => (
+            <span className="pair-block">
+              <span>{tip.word_mother} -</span>
+              <span>{tip.word_target}</span>
+            </span>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tooltip;
