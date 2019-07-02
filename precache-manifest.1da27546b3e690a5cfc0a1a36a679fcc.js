cce8a967d3952807089ba31b671ad91a1807fca2
Your Name <nazik_111@mail.ru>
2019-07-02 21:37:13 +0600

    Updates

---
+++ precache-manifest.1da27546b3e690a5cfc0a1a36a679fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ed9a5ccd0fd2fa8bb6fb30d249e58f",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "91a53e428f27df9de4c6",
+    "url": "/app/static/css/main.54e072eb.chunk.css"
+  },
+  {
+    "revision": "784c69cfa461cef251ae",
+    "url": "/app/static/js/2.301d3a12.chunk.js"
+  },
+  {
+    "revision": "91a53e428f27df9de4c6",
+    "url": "/app/static/js/main.9e4ca604.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "7c9c80a1782979bd81045f49b5be1492",
+    "url": "/app/static/media/help.7c9c80a1.svg"
+  }
+]);
