daae4d4198c4e71efd7fa78ef82590a327a052da
nazimkazim <nazik_111@mail.ru>
2019-09-03 12:35:08 +0600

    add "compare string functionality"

--- src/DialogueShuffleFrameClick.js
+++ src/DialogueShuffleFrameClick.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {uid} from 'react-uid';
 
 
+const pushArr = [];
 class DialogueShuffleFrame extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
-            value:'car'
+            pushArr:[]
         }
+        this.writeSometihng = this.writeSometihng.bind(this)
+    }
+    
+    writeSometihng(e) {
+        e.preventDefault()
+        pushArr.push(e.target.value)
+        console.log(pushArr)
+    }
+
+    checkLines(arr, lines) {
+        let joinedStr = arr.join(' ');
+        //console.log(joinedStr);
+        lines[0].parts.map((obj) => {
+            let line = obj.words
+            console.log(joinedStr == line)
+        })
     }
 
     formatWords(words) {
         const splittedWords = words.split(' ')
         const shuffledArray = this.shuffle(splittedWords)
         return (
-            shuffledArray.map((word, index) => (
+            shuffledArray.map((word) => (
                 <>
-                    <input className="word-to-drop-input" value={this.state.value} onChange={() => {console.log('clicked')}} />
+                    <input className="word-to-drop-input" onBlur={this.writeSometihng} size={2} />
                     <CopyToClipboard text={word}>
-                        <span key={index} value={word} className="word-to-drop">{word}</span>
+                        <span key={uid(word)} value={word} className="word-to-drop">{word}</span>
                     </CopyToClipboard>
                 </>
-                
             ))
         )
     }
@@ -40,20 +56,23 @@ class DialogueShuffleFrame extends Component {
 
     render() {
         const {lines} = this.props
-        //console.log(this.props)
         const shuffles = lines[0].parts && (
             lines[0].parts.map((element,i) => (
+                <>
                     <li className="line" key={i}><span>{element.speaker}{": "}</span><span>{this.formatWords(element.words)}</span></li>
+                    <div type="button" onClick={() => {this.checkLines(pushArr, lines)}} style={{color:'white'}}>check</div>
+                </>
             ))
         )
 
         return (
-            <div>
-                <h1 className="centered" style={{color:'white'}}>Dialogue shuffle frame</h1>
-                <ul>
-                    {shuffles}
-                </ul>
-            </div>
+                <>
+                    <h1 className="centered" style={{color:'white'}}>Dialogue shuffle frame</h1>
+                    <ul>
+                        {shuffles}
+                    </ul>
+                    <button className="grow_box">Check</button>
+                </>
         )
     }
 }
