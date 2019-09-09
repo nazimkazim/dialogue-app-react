cceb4e0153ad109cc4814476b6c4d79e7c1d7847
nazimkazim <nazik_111@mail.ru>
2019-09-09 13:40:08 +0600

    add show text

--- src/DialogueShuffleFrameClick.js
+++ src/DialogueShuffleFrameClick.js
@@ -1,35 +1,37 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { uid } from "react-uid";
-import { MdDoneAll, MdCake } from "react-icons/md";
+//import { MdDoneAll, MdCake } from "react-icons/md";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
+import { Button } from "react-bulma-components/full";
 
 const Container = styled.div`
   display: flex;
   overflow-y: hidden;
   margin: 1em 0;
+  padding-top:0;
   color: #fff;
   width:90%;
   height: 500px;
-  border: 1px solid #333;
+  /* border: 1px solid #333; */
   align-items: center;
 `;
 
 const LeftHalf = styled.div`
-  background-color: #f1f1f1;
+  /* background-color: #f1f1f1; */
   flex: 5;
   height: 100%;
   overflow-y: scroll;
 `;
 
 const RightHalf = styled.div`
-  background-color: #f1f1f1;
+  /* background-color: #f1f1f1; */
   flex: 1;
 `;
 
 const ListContainer = styled.ul`
-  background: yellow;
+  background: white;
 `;
 
 const ListItem = styled.div`
@@ -47,10 +49,10 @@ const NamePlate = styled.div`
   height: auto;
   padding: 0.3rem;
   font-size: 14px;
-  position: absolute;
+  position: relative;
+  background-color:red;
   top: 0;
   left: 0;
-  background-color: red;
 `;
 
 const NamePlateContainer = styled.div`
@@ -60,27 +62,54 @@ const NamePlateContainer = styled.div`
 `;
 
 const WordsContainer = styled.div`
+  display:flex;
+  align-items: center;
+  flex-direction:column;
   flex: 8;
   min-height: 100%;
   background-color: pink;
 `;
 
-const CheckLinesBox = styled.span`
-  flex: 1;
-  background-color: blue;
-`;
-
-const ClearInputBox = styled.span`
-  flex: 1;
-  background-color: green;
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  flex: auto;
 `;
 
 const InputContainer = styled.span`
+  margin-top:0.5rem;
   display:block;
+  width:80%;
+  & > input {
+    width:100%;
+  }
 `
 
 const Words = styled.span`
   display:block;
+  margin-top:.5rem;
+  margin-bottom:.5rem;
+`
+const WordToDrop = styled.span`
+  margin-right: 1rem;
+  padding: 0.3rem;
+  font-size: 0.9rem;
+  display: block;
+  border: 1px solid rgb(0, 255, 221);
+  background: #2c3e50;
+  cursor: pointer;
+  display: inline-block;
+`;
+
+const ShowContainer = styled.div`
+  min-height:50px;
+  min-width:50%;
+  background-color:blue
+`
+
+const ShowText = styled.p`
+  display:none;
 `
 
 class DialogueShuffleFrame extends Component {
@@ -90,34 +119,32 @@ class DialogueShuffleFrame extends Component {
       showCorrect: false,
       preview: "",
       inputAnswer: '',
-      points: 0
+      points: 0    
     };
     this.writeSometihng = this.writeSometihng.bind(this);
+    this.clear = this.clear.bind(this);
+    this.showPhrase = this.showPhrase.bind(this);
   }
 
   writeSometihng(e) {
     e.preventDefault();
-
     this.setState(
       {
         inputAnswer: (e.target.value)
       },
       function() {
         this.preview(this.state.inputAnswer);
-        console.log(this.state.inputAnswer);
       }
     );
   }
 
   checkLines(str, lines) {
-    
-    console.log(str);
     lines[0].parts.map(obj => {
       let line = obj.words;
       if (str.trim() === line) {
         this.setState({
           showCorrect: true,
-          inputAnswer: [],
+          inputAnswer: "",
           points: this.state.points + 80
         });
       } else {
@@ -146,7 +173,7 @@ class DialogueShuffleFrame extends Component {
 
   reset() {
     this.setState({
-      inputAnswer: []
+      inputAnswer: ''
     });
     this.setState({
       preview: ""
@@ -154,15 +181,23 @@ class DialogueShuffleFrame extends Component {
     console.log("clicked");
   }
 
+  showPhrase(e) {
+    console.log(e.target.parentNode.parentNode.children[1].children[2].children[0])
+    e.target.parentNode.parentNode.children[1].children[2].children[0].classList.toggle(
+      'show'
+    )
+  }
+
+
   formatWords(words) {
     const splittedWords = words.split(" ");
     const shuffledArray = this.shuffle(splittedWords);
-    return shuffledArray.map((word, index) => (
+    return shuffledArray.map((word) => (
       <>
         <CopyToClipboard text={word}>
-          <span key={uid(word)} value={word} className="word-to-drop">
+          <WordToDrop key={uid(word)} value={word}>
             {word}
-          </span>
+          </WordToDrop>
         </CopyToClipboard>
       </>
     ));
@@ -194,29 +229,41 @@ class DialogueShuffleFrame extends Component {
 
           <WordsContainer>
             <InputContainer>
-            <input
-              className="word-to-drop-input"
-              id={i}
-              ref="target"
-              onChange={this.writeSometihng}
-              size={2}
-            />
+              <input
+                className="word-to-drop-input"
+                id={i}
+                ref="target"
+                onChange={this.writeSometihng}
+                size={2}
+              />
             </InputContainer>
             <Words>{this.formatWords(element.words)}</Words>
+            <ShowContainer>
+              <ShowText>{element.words}</ShowText>
+            </ShowContainer>
           </WordsContainer>
 
-          <CheckLinesBox
-            type="button"
-            onClick={() => {
-              this.checkLines(this.state.inputAnswer, lines);
-            }}
-            style={{ color: "white" }}
-          >
-            <MdDoneAll />
-          </CheckLinesBox>
-          <ClearInputBox type="button" onClick={this.clear}>
-            <MdCake />
-          </ClearInputBox>
+          <ButtonContainer>
+            <Button
+              color="success"
+              onClick={() => {
+                this.checkLines(this.state.inputAnswer, lines);
+              }}
+              size="small"
+            >
+              Check
+            </Button>
+            <Button color="warning" onClick={this.clear} size="small">
+              Clear
+            </Button>
+            <Button
+              color="success"
+              onClick={this.showPhrase}
+              size="small"
+            >
+            Show Phrase
+            </Button>
+          </ButtonContainer>
         </ListItem>
       ));
 
