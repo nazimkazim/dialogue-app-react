397c460f1286b0018ed7265335af4b7b15e18901
nazimkazim <nazik_111@mail.ru>
2019-10-28 01:59:03 +0600

    add shuffle

--- src/MatchImageToWord.js
+++ src/MatchImageToWord.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Grid, Image, Segment, List, Input } from "semantic-ui-react";
+const _ = require("lodash");
 
 class MatchImageToWord extends Component {
   constructor(props) {
     super(props);
     this.state = {
       set: [],
+      shuffledWords: [],
       inputAnswer: "",
       score: 0
     };
@@ -13,17 +15,30 @@ class MatchImageToWord extends Component {
 
   componentDidMount() {
     //console.log(this.props);
+
+    const shuffledWords = this.props.lines.map(item => {
+      return item.parts.map(i => {
+        return {
+          word: i.word,
+          id: i.id
+        };
+      });
+    });
+
+    //console.log(shuffledWords);
     this.setState({
-      set: this.props
+      set: this.props,
+      shuffledWords: _.shuffle(shuffledWords[0])
     });
   }
   render() {
     //console.log(this.state.set.lines);
+    console.log(this.state.shuffledWords);
     const data =
       this.state.set.lines &&
       this.state.set.lines.map(item =>
         item.parts.map(i => (
-          <Grid.Column>
+          <Grid.Column key={i.id}>
             <Segment>
               <Image
                 src={i.image}
@@ -44,9 +59,11 @@ class MatchImageToWord extends Component {
       this.state.set.lines &&
       this.state.set.lines.map(item =>
         item.parts.map(i => (
-          <List.Item>
-            {i.word}
-            <Input />
+          <List.Item key={i.word}>
+            <span style={{ display: "inline", marginRight: "5px" }}>
+              {i.word}
+            </span>
+            <Input style={{ width: "40px" }} />
           </List.Item>
         ))
       );
@@ -58,7 +75,7 @@ class MatchImageToWord extends Component {
         </Grid.Column>
         <Grid.Column width={12}>
           <Grid>
-            <Grid.Row relaxed columns={4}>
+            <Grid.Row relaxed="true" columns={4}>
               {data}
             </Grid.Row>
           </Grid>
