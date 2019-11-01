47623cabde44734b399bdcf0b9f9dc83e0d8a480
nazimkazim <nazik_111@mail.ru>
2019-11-01 18:19:44 +0600

    add yandex dictionary API

--- src/MatchImageToWord.js
+++ src/MatchImageToWord.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Grid, Image, Segment, List, Input } from "semantic-ui-react";
+import { Grid, Image, Segment, List, Input, Button } from "semantic-ui-react";
+import axios from "axios";
 const _ = require("lodash");
 
 class MatchImageToWord extends Component {
@@ -9,18 +10,51 @@ class MatchImageToWord extends Component {
       set: [],
       shuffledWords: [],
       inputAnswer: "",
-      score: 0
+      score: 0,
+      names: {}
     };
+    this.writeAnswer = this.writeAnswer.bind(this);
   }
 
   componentDidMount() {
     //console.log(this.props);
+    const getTranslations = word => {
+      let translatedWordArr = [];
+      let obj = {};
+      const keyAPI =
+        "dict.1.1.20191101T105247Z.e4967bda9fba183c.2e2703fc28b2d4f5283bb48a004dcb2327b4ba72";
+      axios
+        .get(
+          `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${keyAPI}&lang=en-ru&text=${word}`
+        )
+        .then(res => {
+          //console.log(res);
+
+          if (res.data.def.length > 0) {
+            let translatedWord = res.data.def[0].tr;
+            let pronunciation = res.data.def[0].ts;
+
+            translatedWord.forEach(word => {
+              translatedWordArr.push(word.text);
+            });
+
+            obj.translatedWordArr = translatedWordArr;
+            obj.pronunciation = pronunciation;
+          } else {
+            console.log("no word exist");
+          }
+        });
+
+      return obj;
+    };
 
     const shuffledWords = this.props.lines.map(item => {
       return item.parts.map(i => {
         return {
           word: i.word,
-          id: i.id
+          id: i.id,
+          isCorrect: false,
+          translation: getTranslations(i.word)
         };
       });
     });
@@ -31,9 +65,20 @@ class MatchImageToWord extends Component {
       shuffledWords: _.shuffle(shuffledWords[0])
     });
   }
+
+  checkAnswers(arg1, arg2) {
+    console.log(arg1, arg2);
+  }
+
+  writeAnswer(e) {
+    e.preventDefault();
+    this.setState({ inputAnswer: e.target.value });
+    //console.log(this.state.name);
+  }
   render() {
     //console.log(this.state.set.lines);
     console.log(this.state.shuffledWords);
+    //console.log(this.state.names);
     const data =
       this.state.set.lines &&
       this.state.set.lines.map(item =>
@@ -56,22 +101,36 @@ class MatchImageToWord extends Component {
       );
 
     const words =
-      this.state.set.lines &&
-      this.state.set.lines.map(item =>
-        item.parts.map(i => (
-          <List.Item key={i.word}>
-            <span style={{ display: "inline", marginRight: "5px" }}>
-              {i.word}
-            </span>
-            <Input style={{ width: "40px" }} />
-          </List.Item>
-        ))
-      );
+      this.state.shuffledWords &&
+      this.state.shuffledWords.map((item, i) => (
+        <List.Item key={item.word}>
+          <List.Content floated="right">
+            <Input style={{ width: "40px" }} onChange={this.writeAnswer} />
+          </List.Content>
+          <List.Content style={{ marginLeft: "10px" }}>
+            {item.word}
+          </List.Content>
+        </List.Item>
+      ));
     //console.log(data);
     return (
       <Grid>
         <Grid.Column width={4}>
-          <List>{words}</List>
+          <List divided>{words}</List>
+          <div>
+            <Button
+              color="twitter"
+              size="big"
+              onClick={() => {
+                this.checkAnswers(
+                  this.state.inputAnswer,
+                  this.state.shuffledWords
+                );
+              }}
+            >
+              Check
+            </Button>
+          </div>
         </Grid.Column>
         <Grid.Column width={12}>
           <Grid>
